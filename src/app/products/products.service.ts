9529e0fb4e30745a539e9e078934e92a974bf23a
Yauheni_Tsurhanau <Yauheni_Tsurhanau@epam.com>
2024-03-22 18:39:14 +0400

    feat: add product service link

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -64,7 +64,7 @@ export class ProductsService extends ApiService {
     }
 
     const url = this.getUrl('bff', 'products');
-    return this.http.get<Product[]>(url);
+    return this.http.get<any>(url).pipe(map((response) => response.products));
   }
 
   getProductsForCheckout(ids: string[]): Observable<Product[]> {
